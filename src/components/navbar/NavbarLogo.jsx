ab12944c9f72f45c40607dc8a2ae8d159ee249ca
inspireotech <inspireotech@gmail.com>
2025-01-12 05:41:57 +0500

    hanif

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
       Mohammed Hanif
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        Hanif
+        MF
       </h1>
     </div>
   );
